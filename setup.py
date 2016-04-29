82d74d766a45050db883c8460d179c3d652d9a3f
aulloa <alvarouc@gmail.com>
2016-04-29 16:21:18 -0600

    added setup info

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'amfm',
+  packages = ['amfm'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library for multidimensional AM-FM demodulation',
+  author = 'Alvaro Ulloa',
+  author_email = 'alvarouc@gmail.com',
+  url = 'https://github.com/alvarouc/amfm', # use the URL to the github repo
+  download_url = 'https://github.com/alvarouc/amfm/tarball/0.1', # I'll explain this in a second
+  keywords = ['amfm', 'non-stationary', 'signal processing'], # arbitrary keywords
+  classifiers = [],
+)
